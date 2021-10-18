0e6522372f117e2fe280cd4df27b2f8294a7eb3e
Etrigan Snowball <esnowbal@student.21-school.ru>
2021-10-18 23:11:17 +0300

    123

--- src/main/java/ru/tuanviet/javabox/StringZipper.java
+++ src/main/java/ru/tuanviet/javabox/StringZipper.java
@@ -5,72 +5,52 @@ import java.util.Iterator;
 import java.util.List;
 
 public class StringZipper<T> implements Iterable<String> {
-    private final Iterator<T>[] iterators;
+    private final Iterator<?>[] iterators;
     private final List<String> zippedStrings = new ArrayList<>();
-    private final int zippedStrCount;
 
     @SafeVarargs
     StringZipper(Iterable<T>... iterable) {
+        if (iterable == null) {
+            throw new IllegalArgumentException("Null argument passed");
+        }
         if (iterable.length == 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("No arguments passed");
         }
         iterators = new Iterator[iterable.length];
         for (int i = 0; i < iterators.length; ++i) {
             iterators[i] = iterable[i].iterator();
         }
-        while (iteratorsHasNext()) {
-            zipNextIterator();
-        }
-        zippedStrCount = zippedStrings.size();
-    }
-
-    private boolean iteratorsHasNext() {
-        boolean result = true;
-        for (Iterator<T> it : iterators) {
-            result = result & it.hasNext();
-        }
-        return result;
-    }
-
-    private void zipNextIterator() {
-        StringBuilder builder = new StringBuilder();
-        for (Iterator<T> it : iterators) {
-            builder.append(it.next()).append(" ");
-        }
-        zippedStrings.add(builder.toString());
-    }
-
-    public void printZippedStrings() {
-        for (String str : zippedStrings) {
-            System.out.println(str);
-        }
     }
 
     @Override
     public Iterator<String> iterator() {
-        return new StringZipperIterator(zippedStrings, zippedStrCount);
+        return new StringZipperIterator(iterators);
     }
 }
 
 class StringZipperIterator implements Iterator<String> {
 
-    private int iteratorCurrent = 0;
-    private List<String> zippedStrings;
-    private int zippedStrCount;
+    private final Iterator<?>[] iterators;
 
-    public StringZipperIterator(List<String> zippedStrings, int zippedStrCount) {
-
-        this.zippedStrings = zippedStrings;
-        this.zippedStrCount = zippedStrCount;
+    public StringZipperIterator(Iterator<?>[] iterators) {
+        this.iterators = iterators;
     }
 
     @Override
     public boolean hasNext() {
-        return zippedStrCount - 1 >= iteratorCurrent;
+        boolean result = true;
+        for (Iterator<?> it : iterators) {
+            result &= it.hasNext();
+        }
+        return result;
     }
 
     @Override
     public String next() {
-        return zippedStrings.get(iteratorCurrent++);
+        StringBuilder builder = new StringBuilder();
+        for (Iterator<?> it : iterators) {
+            builder.append(it.next()).append(" ");
+        }
+        return builder.toString();
     }
 }
--- src/test/java/ru/tuanviet/javabox/StringZipperTest.java
+++ src/test/java/ru/tuanviet/javabox/StringZipperTest.java
@@ -4,31 +4,66 @@ import org.junit.Test;
 
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StringZipperTest {
 
-    @Test
-    public void someTest() {
-        List<String> a = new ArrayList<>();
-        Vector<Long> b = new Vector<>();
-        Set<Character> c = new HashSet<>();
-        a.add("a");
-        a.add("b");
-        a.add("c");
-        a.add("c");
-        a.add("c");
-        a.add("c");
-        b.add(1L);
-        b.add(2L);
-        b.add(3L);
-        c.add('A');
-        c.add('B');
-        c.add('C');
-        c.add('D');
-        StringZipper<Integer> test = new StringZipper(a, b, c);
 
-        for (String str : test) {
-            System.out.println(str);
-        }
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExcepitionOnEmptyConstructor() {
+        new StringZipper<>();
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExcepitionOnNull() {
+        new StringZipper<>(null);
+    }
+
+    @Test
+    public void shouldIterateOverOneCollection() {
+        final List<Long> testList = new ArrayList<>();
+
+        testList.add(1L);
+        testList.add(2L);
+        testList.add(3L);
+        testList.add(4L);
+
+        StringZipper<Long> test = new StringZipper<>(testList);
+
+        String actual = "";
+        String expected = "1 2 3 4 ";
+
+        for (String s : test) {
+            actual = String.join("", actual, s);
+        }
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldCheckStringsCount() {
+        final List<Integer> testList = new ArrayList<>();
+        final Set<Integer> testSet = new HashSet<>();
+
+        testList.add(11);
+        testList.add(22);
+        testList.add(33);
+        testList.add(44);
+        testSet.add(1);
+        testSet.add(2);
+        testSet.add(3);
+
+        StringZipper<Integer> test = new StringZipper<>(testList, testSet);
+        int actual = 0;
+        int expected = 3;
+
+        Iterator<String> it = test.iterator();
+
+        while (it.hasNext()) {
+            it.next();
+            actual++;
+        }
+
+        assertThat(actual).isEqualTo(expected);
+    }
 }
